d5ae8330a9b6444614b4f65de64ce212d253069d
Ygor Furtado <88071482+YgorFurtado@users.noreply.github.com>
2023-07-05 19:42:02 -0300

    Add files via upload

--- Gráfico/Graficos do Aluminio.py
+++ Gráfico/Graficos do Aluminio.py
@@ -23,13 +23,13 @@ coef_ang_paq, coef_lin_paq,a ,b ,c = sci.linregress(log_paquimetro,log_massa)
 coef_ang_reg, coef_lin_reg,d ,e ,f = sci.linregress(log_regua,log_massa)
 y_paq = coef_ang_paq*1 + coef_lin_paq
 y_reg= coef_ang_reg*1 + coef_lin_reg
-print("O Coeficiente angular medido no paquimetro é:" + str(coef_ang_paq))
+print("O Coeficiente angular medido no paquímetro é:" + str(coef_ang_paq))
 print("O Coeficiente angular medido na régua é:" + str(coef_ang_paq))
 
 #Cria os gráficos Diâmetro x Massa:
 plt.subplot(221)
-plt.errorbar(diam_r,massa,xerr = erro_r,fmt = "D:",label="Regua")
-plt.errorbar(diam_p,massa,xerr = erro_p,fmt = "s--",label="Paquimetro")
+plt.errorbar(diam_r,massa,xerr = erro_r,fmt = "D:",label="Régua")
+plt.errorbar(diam_p,massa,xerr = erro_p,fmt = "s--",label="Paquímetro")
 plt.grid()
 plt.xlabel("Diametro(mm)")                         
 plt.ylabel("Massa(g)")
@@ -38,8 +38,8 @@ plt.legend()
 
 #Cria os gráficos LogxLog - Diâmetro x Massa:
 plt.subplot(222)
-plt.plot(log_regua,log_massa,"s--",label="Regua",)
-plt.plot(log_paquimetro,log_massa,"D:",label="Paquimetro",)
+plt.plot(log_regua,log_massa,"s--",label="Régua",)
+plt.plot(log_paquimetro,log_massa,"D:",label="Paquímetro",)
 plt.grid()
 plt.xlabel("Log(Diametro)")                         
 plt.ylabel("Log(Massa)")
@@ -50,12 +50,12 @@ plt.ylim(-4.95,1.2)
 
 #Cria gráfico logXlog ajeitados
 plt.subplot(212)
-plt.axline((1,y_reg),slope=coef_ang_reg,ls = (5, (10, 3)),label = "Regua")
-plt.axline((1,y_paq),slope=coef_ang_paq,ls = (0, (3, 1, 1, 1, 1, 1)),label="Paquimetro",color = "orange")
+plt.axline((1,y_reg),slope=coef_ang_reg,ls = (5, (10, 3)),label = "Régua")
+plt.axline((1,y_paq),slope=coef_ang_paq,ls = (0, (3, 1, 1, 1, 1, 1)),label="Paquímetro",color = "orange")
 plt.grid()
 plt.xlabel("Log(Diametro)")                         
 plt.ylabel("Log(Massa)")
-plt.title("Log x Log Ajeitado")
+plt.title("Log x Log Ajustado")
 plt.legend()
 plt.xlim(1,3.75)
 plt.ylim(-4.95,1.2)
